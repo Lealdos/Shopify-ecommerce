a862e127742ba709783892887c8675770b54cb34
j3leonardodelgado <j3leonardodelgado@gmail.com>
2024-02-21 14:58:01 +0000

    add services files

---
+++ src/api/route.ts
@@ -0,0 +1,6 @@
+export async function GET() {
+    const message = "hello world"
+    
+ return Response.json({message})
+    
+}
--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,26 +1,14 @@
 import Image from 'next/image';
 import { Product } from 'app/models/interface/products';
-const getProducts = async () => {
-    try {
-        const response = await fetch(
-            `${process.env.SHOPYFY_HOSTNAME}/admin/api/2023-10/products.json`,
-            {
-                headers: new Headers({
-                    'X-Shopify-Access-Token':
-                        process.env.SHOPIFY_AUTH_TOKEN || '',
-                }),
-            }
-        );
 
-        const { products } = await response.json();
-        return products;
-    } catch (error) {
-        console.error(error);
-    }
-};
+import { getProducts } from 'app/services/shopify';
+
+
+
 
 export async function MainProducts() {
-    const products = await getProducts();
+    const products = await getProducts()
+    
     return (
         <section className='w-full h-full m-2'>
             <h3 className='text-3xl text-sky-500 text-center italic my-10'>
@@ -28,7 +16,7 @@ export async function MainProducts() {
             </h3>
 
             <div className='flex flex-col gap-4 mx-4 md:grid md:grid-cols-2 lg:grid-cols-3 md:grid-rows-3 grid-rows-[200px_minmax(900px,_1fr)_100px]'>
-                {products.map((product: Product) => {
+                {products?.map((product: Product) => {
                     const imageSrc = product.images[0].src;
                     return (
                         <article
