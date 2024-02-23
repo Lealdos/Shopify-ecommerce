accab84677dbe66f0525ceebff85dbd428a53ca2
j3leonardodelgado <j3leonardodelgado@gmail.com>
2024-02-23 14:16:00 +0000

    fixed grid store justify

--- src/components/Store/ProductsWrapper/ProductsWrapper.tsx
+++ src/components/Store/ProductsWrapper/ProductsWrapper.tsx
@@ -6,7 +6,7 @@ interface ProductsWrapperProps {
 
 export const ProductsWrapper = ({ products }: ProductsWrapperProps) => {
   return (
-      <div className="min-w-full justify-center items-center flex flex-col gap-4  md:grid md:grid-cols-2 lg:grid-cols-3 md:grid-rows-3 grid-rows-[200px_minmax(900px,_1fr)_100px]">
+      <div className="min-w-full justify-center items-center flex flex-col gap-4 md:justify-items-center md:grid md:grid-cols-2 lg:grid-cols-3 md:grid-rows-3 grid-rows-[200px_minmax(900px,_1fr)_100px]">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
