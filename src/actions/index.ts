65cc231f79fbea6bded1e67a37a5c5b75ed01103
Jose Delgado <j3leonardodelgado@gmail.com>
2024-03-03 20:41:50 -0500

    feature: create user at shopify store

---
+++ src/actions/index.ts
@@ -0,0 +1,44 @@
+'use server';
+
+import { GraphQLClientSingleton } from '@/graphql';
+import { createUserMutation } from '@/graphql/mutations/createUserMutation';
+import { createAccessToken } from '@/utils/auth/createAccessToken';
+import { redirect } from 'next/navigation';
+
+//falta encriptar contraseña y manejo de errores
+
+type GraphQLResponse = {
+    customerCreate: {
+        customerUserErrors: any[];
+        customer: any;
+    };
+};
+
+export const handleCreateUser = async (formData: FormData) => {
+    const graphQLClient = GraphQLClientSingleton.getInstance().getClient();
+    const formDataObj = Object.fromEntries(formData);
+    console.log(Object.fromEntries(formData.entries()));
+
+    const variables = {
+        input: {
+            ...formDataObj,
+            phone: '+1' + formDataObj.phone,
+            acceptsMarketing: formData.get('acceptsMarketing') === 'off',
+        },
+    };
+
+    const { customerCreate } = await graphQLClient.request<GraphQLResponse>(
+        createUserMutation,
+        variables
+    );
+    const { customerUserErrors, customer } = customerCreate;
+    if (customer?.lastName) {
+        await createAccessToken(
+            customer.email,
+            formData.get('password') as string
+        );
+        redirect('/store');
+    }
+    console.log('customer: ', customer);
+    console.log('errors: ', customerUserErrors);
+};
--- src/components/SignUp/NewAccountForm/NewUserForm.tsx
+++ src/components/SignUp/NewAccountForm/NewUserForm.tsx
@@ -1,40 +1,37 @@
-"use client";
-import { useState } from "react";
-// import { handleCreateUser } from "app/actions";
+'use client';
+import { handleCreateUser } from '@/actions';
+import { useState } from 'react';
 
+// falta la parte de la confirmacion de la contraseña
 export const NewUserForm = () => {
-  const [errors, setErrors] = useState<string[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
+    const [errors, setErrors] = useState<string[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.target as HTMLFormElement);
-    // handleCreateUser(formData)
-    setLoading(true);
-  };
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setLoading(true);
+        const formData = new FormData(event.target as HTMLFormElement);
+        formData.delete('passwordConfirmation');
 
-  return (
-    <div className="flex flex-col gap-4 items-center justify-center h-screen">
-      <h1 className="">New Account</h1>
-      {/* <form className='bg-black flex flex-col gap-4 sm:flex sm:flex-row md:gap-8 '  onSubmit={handleSubmit}>
-        <input className='' type="text" name="first_name" placeholder="Name" disabled={loading} />
-        <input className='' type="text" name="last_name" placeholder="Lastname" disabled={loading} />
-        <input className='' type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" disabled={loading} />
-        <input className='' type="text" name="phone" placeholder="phone number" pattern="^[0-9]*$" disabled={loading} />
-        <input className='' type="password" name="password" placeholder="password" disabled={loading} />
-        <input className='' type="password" name="password_confirmation" placeholder="re-type password" disabled={loading} />
-        {/* <input className='' type="submit" name="submit" value="Crear cuenta" disabled={loading} /> 
-        <button>leo</button>
-      </form> */}
-      <div className="min-w-80 md:w-96 md:max-w-full mx-auto rounded-lg ">
-        <div className="p-6 border border-gray-600 sm:rounded-md bg-gradient-to-tr
+        await handleCreateUser(formData);
+
+        setLoading(false);
+    };
+
+    return (
+        <div className='flex flex-col gap-4 items-center justify-center h-screen'>
+            <h1 className=''>New Account</h1>
+            <div className='min-w-80 md:w-96 md:max-w-full mx-auto rounded-lg '>
+                <div
+                    className='p-6 border border-gray-600 sm:rounded-md bg-gradient-to-tr
          from-blue-900 via-indigo-800 to-emerald-800 rounded shadow-sm shadow-sky-500
-            ">
-          <form onSubmit={handleSubmit} >
-            <label className="block mb-6">
-              <span className="text-gray-300">First name</span>
-              <input
-                className="           
+            '
+                >
+                    <form onSubmit={handleSubmit}>
+                        <label className='block mb-6'>
+                            <span className='text-gray-300'>First name</span>
+                            <input
+                                className='           
                             block
                             w-full
                             mt-1
@@ -48,17 +45,17 @@ export const NewUserForm = () => {
                             bg-transparent
                             placeholder-gray-800
                             text-gray-300
-            "
-                type="text"
-                name="firstName"
-                placeholder="Name"
-                disabled={loading}
-              />
-            </label>
-            <label className="block mb-6">
-              <span className="text-gray-300">Last name</span>
-              <input
-                className="            
+            '
+                                type='text'
+                                name='firstName'
+                                placeholder='Name'
+                                disabled={loading}
+                            />
+                        </label>
+                        <label className='block mb-6'>
+                            <span className='text-gray-300'>Last name</span>
+                            <input
+                                className='            
                             block
                             w-full
                             mt-1
@@ -71,21 +68,21 @@ export const NewUserForm = () => {
                             focus:ring-opacity-50
                             bg-transparent
                             placeholder-gray-800
-                            text-gray-300"
-                type="text"
-                name="lastName"
-                placeholder="Lastname"
-                disabled={loading}
-              />
-            </label>
-            <label className="block mb-6">
-              <span className="text-gray-300">Email address</span>
-              <input
-                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                disabled={loading}
-                name="email"
-                type="email"
-                className="
+                            text-gray-300'
+                                type='text'
+                                name='lastName'
+                                placeholder='LastName'
+                                disabled={loading}
+                            />
+                        </label>
+                        <label className='block mb-6'>
+                            <span className='text-gray-300'>Email address</span>
+                            <input
+                                pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
+                                disabled={loading}
+                                name='email'
+                                type='email'
+                                className='
                               block
                               w-full
                               mt-1
@@ -99,15 +96,17 @@ export const NewUserForm = () => {
                               bg-transparent
                               placeholder-gray-800
                               text-gray-300
-          "
-                placeholder="joe.bloggs@example.com"
-                required
-              />
-            </label>
-            <label className="block mb-6">
-              <span className="text-gray-300">Cellphone number</span>
-              <input
-                className="
+          '
+                                placeholder='joe.bloggs@example.com'
+                                required
+                            />
+                        </label>
+                        <label className='block mb-6'>
+                            <span className='text-gray-300'>
+                                Cellphone number
+                            </span>
+                            <input
+                                className='
                             block
                             w-full
                             mt-1
@@ -121,21 +120,21 @@ export const NewUserForm = () => {
                             bg-transparent
                             placeholder-gray-800
                             text-gray-300
-                "
-                type="text"
-                name="phone"
-                placeholder="phone number"
-                pattern="^[0-9]*$"
-                disabled={loading}
-              />
-            </label>
-            <label className="block mb-6">
-              <span className="text-gray-300">Password</span>
-              <input
-                disabled={loading}
-                name="password"
-                type="password"
-                className="
+                '
+                                type='text'
+                                name='phone'
+                                placeholder='phone number'
+                                pattern='^[0-9]*$'
+                                disabled={loading}
+                            />
+                        </label>
+                        <label className='block mb-6'>
+                            <span className='text-gray-300'>Password</span>
+                            <input
+                                disabled={loading}
+                                name='password'
+                                type='password'
+                                className='
                             block
                             w-full
                             mt-1
@@ -149,20 +148,22 @@ export const NewUserForm = () => {
                             bg-transparent
                             placeholder-gray-600
                             text-gray-300
-                          "
-                minLength={6}
-                placeholder="(must be 6+ chars)"
-                required
-              />
-            </label>
-            <div className="mb-6">
-              <div className="mt-2">
-                <label className="block mb-6">
-                  <span className="text-gray-300">Re-enter Password</span>
-                  <input
-                    name="passwordConfirmation"
-                    type="password"
-                    className="
+                          '
+                                minLength={6}
+                                placeholder='(must be 6+ chars)'
+                                required
+                            />
+                        </label>
+                        <div className='mb-6'>
+                            <div className='mt-2'>
+                                <label className='block mb-6'>
+                                    <span className='text-gray-300'>
+                                        Re-enter Password
+                                    </span>
+                                    <input
+                                        name='passwordConfirmation'
+                                        type='password'
+                                        className='
                                   block
                                   w-full
                                   mt-1
@@ -176,22 +177,24 @@ export const NewUserForm = () => {
                                   bg-transparent
                                   placeholder-gray-600
                                   text-gray-300
-          "
-                    minLength={6}
-                    placeholder="(must be 6+ chars)"
-                    required
-                  />
-                </label>
-              </div>
-            </div>
-            <div className="mx-auto
-            flex flex-col justify-center items-center w-6/12">
-              <label > Create account</label>
-              <button
-                type="submit"
-                disabled={loading}
-                name="submit"
-                className="
+          '
+                                        minLength={6}
+                                        placeholder='(must be 6+ chars)'
+                                        required
+                                    />
+                                </label>
+                            </div>
+                        </div>
+                        <div
+                            className='mx-auto
+            flex flex-col justify-center items-center w-6/12'
+                        >
+                            <span> Create account</span>
+                            <button
+                                type='submit'
+                                disabled={loading}
+                                name='submit'
+                                className='
                               h-10
                               px-5
                               text-indigo-100
@@ -201,27 +204,26 @@ export const NewUserForm = () => {
                               duration-150
                               focus:shadow-outline
                               hover:bg-indigo-800
-                            "
-              >
-                Sign Up
-              </button>
+                            '
+                            >
+                                Sign Up
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
 
-          </form>
+            {errors.length > 0 && (
+                <div>
+                    {errors.map((error, index) => {
+                        return (
+                            <p key={error} className=''>
+                                {error}
+                            </p>
+                        );
+                    })}
+                </div>
+            )}
         </div>
-      </div>
-
-      {errors.length > 0 && (
-        <div>
-          {errors.map((error, index) => {
-            return (
-              <p key={index} className="">
-                {error}
-              </p>
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
+    );
 };
