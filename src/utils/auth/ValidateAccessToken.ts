d337bf91b29e0c4df7b64003538b256a7de8fc55
j3leonardodelgado <j3leonardodelgado@gmail.com>
2024-03-09 17:07:33 +0000

    feature: login user

---
+++ src/utils/auth/ValidateAccessToken.ts
@@ -0,0 +1,13 @@
+import { GraphQLClientSingleton } from 'app/graphql'
+import { customerName } from 'app/graphql/queries/customerName'
+import { cookies } from 'next/headers'
+
+export const validateAccessToken = async () => {
+  const cookieStore = cookies()
+  const accessToken = cookieStore.get('accessToken')?.value
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+  const { customer } = await graphqlClient.request<any>(customerName, {
+    customerAccessToken: accessToken
+  })
+  return customer
+}
