1af58134d6a18a2beca4b2a5e86fdd1c2ccc8a4c
j3leonardodelgado <j3leonardodelgado@gmail.com>
2024-03-14 20:53:49 +0000

    clean: login page

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -2,28 +2,6 @@ import { LoginForm } from "@/components/Login";
 
 export default function LoginPage() {
 
-  // async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-  //   event.preventDefault()
-
-  //   const formData = new FormData(event.currentTarget as HTMLFormElement)
-  //   const email = formData.get('email')
-  //   const password = formData.get('password')
-
-  //   const response = await fetch('/api/auth/login', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({ email, password }),
-  //   })
-
-  //   if (response.ok) {
-  //     redirect('/store')
-  //   } else {
-  //     // Handle errors
-  //   }
-  // }
-
-
-
   return (
     <LoginForm/>
   )
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from '@/components/shared/Loader';
 export default function Loading() {
     return (
         <div className='w-full h-full my-56'>
-            <Loader />
+            <Loader/>
         </div>
     );
 }
